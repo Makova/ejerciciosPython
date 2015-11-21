e77be14655a092a27d993fbc3e8bf272e44013cc
makova <makova65@gmail.com>
2015-11-21 22:55:53 +0100

    probando combinaciones

---
+++ conelif1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+edad = int(input("¿Cuántos años tienes?"))
+if edad >= 18:
+	print ("Eres mayor de edad")
+elif edad >= 17:
+	print("Eres menor de edad")
+elif edad <= 0:
+	print("No se puede tener una edad negativa")
+else:
+	print"¿Seguro que tienes", edad, "años?"
