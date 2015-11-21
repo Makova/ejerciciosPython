2f765571c8d15bb8280dee108e45e9d8f1ce6555
makova <makova65@gmail.com>
2015-11-21 21:15:58 +0100

    con if y else

---
+++ seguimosEdades.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+edad = int(input("¿Cuántos años tiene? "))
+if edad < 18:
+    print("Es usted menor de edad")
+    print("Recuerde que está en la edad de aprender")
+else:
+    print("Es usted mayor de edad")
+    print("Recuerde que debe seguir aprendiendo")
+print("¡Hasta la próxima!")
