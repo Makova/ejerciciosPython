6f06336d5e912feb016792d61e3f2c3b5cd344c6
Makova <makova65@gmail.com>
2016-04-21 17:06:38 +0200

    comitear por comitear

--- Libro_Python_Pablo/verdadero_falso.py
+++ Libro_Python_Pablo/verdadero_falso.py
@@ -29,13 +29,13 @@ print resultado
 # 3 no es igual a 2, por lo que es falso
 
 print True and True
-# algo que es "cierto" y cierto" es cierto
+# algo que es "cierto y cierto" es cierto
 
 print True and resultado
-# algo que es "cierto" y falso" es falso
+# algo que es "cierto y falso" es falso
 
 print not True
-# "no cierto" es falso"
+# "no cierto" es falso
 
 print False or resultado
 # algo que es "falso o falso" es falso
