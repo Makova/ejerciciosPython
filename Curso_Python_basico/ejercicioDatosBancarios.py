b19c4480a440f0e24d1ac8dae3e7627768b5f902
Manu <makova65@gmail.com>
2016-05-09 22:03:57 +0200

    Elimino nombre y email

--- Curso_Python_basico/ejercicioDatosBancarios.py
+++ Curso_Python_basico/ejercicioDatosBancarios.py
@@ -3,8 +3,6 @@
 #
 #  ejercicioDatosBancarios.py
 #  
-#  Copyright 2016 Manu <makova65@gmail.com>
-#  
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation; either version 2 of the License, or
